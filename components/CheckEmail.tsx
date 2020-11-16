67ab058f11d8495af0685b344777f5b4f1393380
Travis Hoover <traviskhoover@gmail.com>
2020-11-15 22:38:50 -0600

    Change icon and hook up to HIBP's api to check for compromised emails

--- components/CheckEmail.tsx
+++ components/CheckEmail.tsx
@@ -1,10 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, TouchableOpacity } from 'react-native';
 
 import Colors from '../constants/Colors';
 import { Text, TextInput, View } from './Themed';
 
 export default function CheckEmail({ path }: { path: string }) {
+  const [emailAddress, setEmailAddress] = useState('');
+  const [results, setResults] = useState(null);
+
+  useEffect(() => {
+    if (!emailAddress) {
+      return;
+    }
+    const results = () => fetch(
+        `https://haveibeenpwned.com/api/v3/breachedaccount/${emailAddress}`,
+        {
+          method: "GET",
+          headers: new Headers({
+            Accept: "application/json",
+            "hibp-api-key": "dd439240c4e94b429eb478e497897c64",
+          })
+        }
+    )
+        .then(res => res.json())
+        .then(response => {
+          setResults(response);
+        })
+        .catch(error => console.log(error));
+
+    let debouncer = setTimeout(() => {
+      results();
+    }, 1500);
+    return () => {
+      clearTimeout(debouncer);
+    }
+  }, [emailAddress]);
+
   return (
     <View>
       <View style={styles.getStartedContainer}>
@@ -18,8 +49,23 @@ export default function CheckEmail({ path }: { path: string }) {
 
       <View style={styles.helpContainer}>
         <TouchableOpacity style={styles.helpLink}>
-          <TextInput style={styles.helpLinkText} lightColor={Colors.light.tint}/>
+          <TextInput
+              style={styles.helpLinkText}
+              lightColor={Colors.light.tint}
+              onChangeText={(value: string) => {
+                setEmailAddress(value)
+              }}
+          />
         </TouchableOpacity>
+      </View>
+
+      <View>
+        <Text
+          style={styles.getStartedText}
+          lightColor="rgba(0,0,0,0.8)"
+          darkColor="rgba(255,255,255,0.8)">
+          {results ? JSON.stringify(results) : null}
+        </Text>
       </View>
     </View>
   );
