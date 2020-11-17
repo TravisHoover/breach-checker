2b570ccb7816dbc52d24ec63fc75b1992b3c2ab3
Travis Hoover <traviskhoover@gmail.com>
2020-11-16 23:40:36 -0600

    getting passwords started

--- components/CheckPassword.tsx
+++ components/CheckPassword.tsx
@@ -1,27 +1,67 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, TouchableOpacity } from 'react-native';
-
+import sha1 from 'sha1';
 import Colors from '../constants/Colors';
 import { Text, TextInput, View } from './Themed';
 
-export default function CheckEmail({ path }: { path: string }) {
-  return (
-    <View>
-      <View style={styles.getStartedContainer}>
-        <Text
-          style={styles.getStartedText}
-          lightColor="rgba(0,0,0,0.8)"
-          darkColor="rgba(255,255,255,0.8)">
-          Has your password been compromised? Find out!
-        </Text>
-      </View>
+export default function CheckPassword({ path }: { path: string }) {
+  const [password, setPassword] = useState('');
+  const [results, setResults] = useState('');
 
-      <View style={styles.helpContainer}>
-        <TouchableOpacity style={styles.helpLink}>
-          <TextInput style={styles.helpLinkText} lightColor={Colors.light.tint}/>
-        </TouchableOpacity>
+  useEffect(() => {
+    if (!password) {
+      return;
+    }
+    const hashedPassword: string = sha1(password);
+    const results = () => fetch(
+        `https://api.pwnedpasswords.com/range/${hashedPassword.slice(0,5)}`
+    ).then(response => response.text())
+        // @ts-ignore
+        .then(data => setResults(data))
+        .catch(error => console.log(error));
+
+    let debouncer = setTimeout(() => {
+      results();
+    }, 1500);
+    return () => {
+      clearTimeout(debouncer);
+    }
+  }, [password]);
+
+
+  const re1 = new RegExp(`${sha1(password).toUpperCase()}:.`,"g");
+  return (
+      <View>
+        <View style={styles.getStartedContainer}>
+          <Text
+              style={styles.getStartedText}
+              lightColor="rgba(0,0,0,0.8)"
+              darkColor="rgba(255,255,255,0.8)">
+            Has your password been compromised? Find out!
+          </Text>
+        </View>
+
+        <View style={styles.helpContainer}>
+          <TouchableOpacity>
+            <TextInput
+                style={styles.helpLinkText}
+                lightColor={Colors.light.tint}
+                onChangeText={(value: string) => {
+                  setPassword(value)
+                }}
+            />
+          </TouchableOpacity>
+        </View>
+
+        <View>
+          <Text
+              style={styles.getStartedText}
+              lightColor="rgba(0,0,0,0.8)"
+              darkColor="rgba(255,255,255,0.8)">
+            {results ? results.match(re1) : null}
+          </Text>
+        </View>
       </View>
-    </View>
   );
 }
 
@@ -80,5 +120,8 @@ const styles = StyleSheet.create({
   },
   helpLinkText: {
     textAlign: 'center',
+    width: 250,
+    borderColor: 'black',
+    borderBottomWidth: 1.5,
   },
 });
